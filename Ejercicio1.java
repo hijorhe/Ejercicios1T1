924aae9b79699f8088677ee7ce00e102c860125b
hijorhe <92974341+hijorhe@users.noreply.github.com>
2022-05-01 23:36:30 -0500

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,23 @@
+package com;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args)
+    {
+        int  a= 2;
+        long b=123456;
+        double c = 3.10;
+        float d = (float) (20000*5);
+        String e= "ejercicios";
+        char f = 'G';
+        byte g = 4;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+    }
+}
